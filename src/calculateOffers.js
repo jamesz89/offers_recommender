61e47be6227ddd97409a3fb2c98cea4599c069fc
James Zambrano <zambranojamesj@gmail.com>
2022-01-19 11:30:05 -0300

    finish calculateOffers process

--- src/calculateOffers.js
+++ src/calculateOffers.js
@@ -3,27 +3,58 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 //Google Services credentials
-import serviceAccount from './serviceAccount.json'
+import serviceAccount from "./serviceAccount.json";
 
 //init database connection
 initializeApp({
-  credential: cert(serviceAccount)
-})
-const db = getFirestore()
-//Fetch all docs from products collection
-const fetchAllDocuments = () => {
-  const productsRef = db.collection('products')
-  const snapshot = await productsRef.get()
-  return snapshot
-}
-//Start Loop
-//Read a product
-//Read offerPrice and price values
-//Substract price - offerPrice
-//Save result into new doc structure
-//Calculate differential percentage
-//Save result into new doc structure
-//Create new collection products with offers
-//Write each modified doc into collection
-//Keep the same id for new docs
-//End Loop
+  credential: cert(serviceAccount),
+});
+const db = getFirestore();
+
+const fetchAllDocuments = async () => {
+  const productsRef = db.collection("products");
+  const snapshot = await productsRef.get();
+  return snapshot;
+};
+
+const calculatePriceDifference = (price, offerPrice) => {
+  const result = price - offerPrice;
+  if (result <= 0) {
+    return 0;
+  }
+  return result;
+};
+
+const calculatePriceDifferencePercentage = (price, offerPrice) => {
+  const difference = price - offerPrice;
+  return Math.round((difference * 100) / price);
+};
+
+const products = await fetchAllDocuments();
+
+products.forEach(async (product) => {
+  let priceDifference = calculatePriceDifference(
+    product.data().price,
+    product.data().offerPrice
+  );
+  let priceDifferencePercentage = calculatePriceDifferencePercentage(
+    product.data().price,
+    product.data().offerPrice
+  );
+  if (priceDifferencePercentage > 0) {
+    let docRef = db.collection("productsWithOffers").doc(product.id);
+    await docRef.set({
+      brand: product.data().brand,
+      categoryId: product.data().categoryId,
+      imageUrl: product.data().imageUrl,
+      name: product.data().name,
+      offerPrice: product.data().offerPrice,
+      price: product.data().price,
+      sku: product.data().sku,
+      url: product.data().url,
+      priceDifference: priceDifference,
+      priceDifferencePercentage: priceDifferencePercentage,
+    });
+    console.log("adding...", product.data().name);
+  }
+});
