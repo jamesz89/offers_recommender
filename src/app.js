29d028320afeb92903521fcb2bbd4b479384b596
James Zambrano <zambranojamesj@gmail.com>
2022-01-19 17:00:09 -0300

    add offersByCategoryId endpoint

--- src/app.js
+++ src/app.js
@@ -1,43 +1,59 @@
 //Get dependecies
-import express from 'express'
+import express from "express";
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 //Google Services credentials
-import serviceAccount from './serviceAccount.json'
+import serviceAccount from "./serviceAccount.json";
 
 //init database connection
 initializeApp({
-  credential: cert(serviceAccount)
-})
-const db = getFirestore()
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+
+console.log("DB connection established");
 
 //init express server
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 //Routes
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get('/offers', async (req, res) => {
-  let data = []
-  const productsRef = db.collection('productsWithOffers')
-  const snapshot = await productsRef.orderBy('priceDifferencePercentage', 'desc').limit(12).get()
+app.get("/offers", async (req, res) => {
+  let data = [];
+  const productsRef = db.collection("productsWithOffers");
+  const snapshot = await productsRef
+    .orderBy("priceDifferencePercentage", "desc")
+    .limit(12)
+    .get();
   if (snapshot.empty) {
     return res.status(404).json({
-      error: 'not enough products to recommend'
+      error: "not enough products to recommend",
+    });
+  }
+  snapshot.forEach((doc) => data.push(doc.data()));
+  res.json(data);
+});
+
+app.get("/offersByCategory/:id", async (req, res) => {
+  let data = []
+  let id = req.params.id
+  const productsRef = db.collection("productsWithOffers")
+  const snapshot = await productsRef.where('categoryId', '==', id).orderBy("priceDifferencePercentage", "desc").limit(12).get()
+  if (snapshot.empty) {
+    return res.status(404).json({
+      error: "not enough products to recommend",
     })
   }
-  snapshot.forEach(doc => data.push(doc.data()))
-  res.json(data)
-})
-
-app.get('/offersByCategory:id', async (req, res) => {
-  //returns a json with 12 products from an specific category that have offers ordered by amount of difference between offerPrice and price
-})
+  snapshot.forEach((doc) => data.push(doc.data()))
+  res.status(200).json(data)
+});
 
 app.listen(PORT, () => {
-  console.log(`Express started on port ${PORT} `)
-})
+  console.log(`Express started on port ${PORT} `);
+});
